2fbdcf3f9022c40a0fe7bf2bee093b4262f59a9a
theReaLSingularity <no1.coffee.addict@gmail.com>
2026-02-22 20:59:30 -0800

    WIP - adding db stuff

---
+++ docker-db-stuff/final-project/README.txt
@@ -0,0 +1,38 @@
+SmartGrid_CST363_Project
+
+-- Steps to Run the Project --
+
+- Clone project from GitHub: https://github.com/theReaLSingularity/SmartGrid_CST363_Project
+
+- Configure project with Python 3.10 virtual environment
+
+- Ensure all dependencies are installed (pip install -r requirements.txt)
+
+- Ensure Docker is running before starting the process.
+
+Follow the steps below in order:
+
+1. Run weather_date_formatter.py and copy generated files to sql data directory
+(OPTIONAL — only required for full data pipeline recreation.)
+
+2. Run london_calendar_creator.py and copy generated files to sql data directory
+(OPTIONAL — only required for full data pipeline recreation.)
+
+3. Start the Docker services in 'final-project' directory: docker compose up -d
+   and allow 2-3 minutes for data to copy and tables to populate.
+
+4. Run build_features_duckdb.py
+(This creates the features table required for model training.)
+
+5. Optionally, open smart_grid_CST363.sql and uncomment the section marked:
+
+"-- **** REQUIRES FEATURES TABLE CREATED WITH DUCKDB: *****--"
+
+and run uncommented queries.
+
+6. Run model_train_test.py
+(This will train the model and generate predictions.)
+
+Output:
+After successful execution, the model predictions plot will be generated at:
+SmartGrid_CST363_Project/plots/Prediction_vs_Actual.png
---
+++ docker-db-stuff/final-project/sql/init.sql
@@ -0,0 +1,154 @@
+-- Alexander Moore
+-- Bryan Rivera
+-- Lee Cao
+-- CST 363-30
+-- 02/14/2026
+
+
+ 			-- **** docker-compose.yml contents: *****
+-- services:
+--   pg:
+--     image: pgvector/pgvector:pg16
+--     environment:
+--       POSTGRES_USER: smart
+--       POSTGRES_PASSWORD: smart1
+--       POSTGRES_DB: smartgrid_db
+--     ports:
+--       - "5435:5432"
+--     volumes:
+--       - smartgrid_pgdata:/var/lib/postgresql/data
+--       - ./data:/data
+--	 - ./sql:/docker-entrypoint-initdb.d
+
+-- volumes:
+--   smartgrid_pgdata:
+
+ 			-- **** Running Docker compose command: *****
+-- docker compose up -d
+
+ 			-- **** Dropping tables if exist: *****
+DROP VIEW IF EXISTS v_daily_model_input;
+DROP TABLE IF EXISTS avg_consumption;
+DROP TABLE IF EXISTS energy;
+DROP TABLE IF EXISTS weather;
+DROP TABLE IF EXISTS calendar;
+DROP TABLE IF EXISTS features_daily;
+
+
+ 			-- **** Enabling pgvector extension: *****
+CREATE EXTENSION IF NOT EXISTS vector;
+
+
+ 			-- **** Creating energy table: *****
+CREATE TABLE energy (
+	household_id 		VARCHAR(50),
+	date 				DATE,
+	kwhr 				DOUBLE PRECISION,
+	PRIMARY KEY (household_id, date)
+);
+
+COPY energy (household_id, date, kwhr)
+	FROM '/data/london_energy.csv'
+	WITH (FORMAT CSV, HEADER);
+
+DELETE FROM energy 
+	WHERE date >= '2014-02-28' -- anomolous data points on '2014-02-28'
+		OR date < '2012-01-01'; -- too few samples before '2012-01-01' (<500)
+
+
+ 			-- **** Creating weather table: *****
+CREATE TABLE weather (
+	date 				DATE PRIMARY KEY,
+	cloud_cover 		FLOAT,
+	sunshine 			FLOAT,
+	global_radiation 	FLOAT,
+	max_temp 			FLOAT,
+	mean_temp 			FLOAT,
+	min_temp 			FLOAT,
+	precipitation		FLOAT,
+	pressure			FLOAT,
+	snow_depth			FLOAT
+);
+
+COPY weather (	date,
+				cloud_cover,
+				sunshine,
+				global_radiation,
+				max_temp,
+				mean_temp,
+				min_temp,
+				precipitation,
+				pressure,
+				snow_depth)
+	FROM '/data/london_weather.csv'
+	WITH (FORMAT CSV, HEADER);
+	
+DELETE FROM weather
+	WHERE date < '2012-01-01' -- limiting weather data to match available energy data date range
+		OR date > '2014-02-27';
+
+UPDATE weather
+	SET cloud_cover = (SELECT AVG(cloud_cover) -- dealing with NULL value
+					   FROM weather
+					   WHERE date
+					       IN ('2012-03-01', '2012-03-03'))
+	WHERE date = '2012-03-02';
+
+
+
+ 			-- **** Creating calendar table: *****
+CREATE TABLE calendar (
+	date		DATE PRIMARY KEY,
+	dow			INTEGER,
+	day			INTEGER,
+	month		INTEGER,
+	year		INTEGER,
+	doy			INTEGER,
+	is_weekend	INTEGER,
+	is_holiday	INTEGER
+);
+
+COPY calendar (	date,
+				dow,
+				day,
+				month,
+				year,
+				doy,
+				is_weekend,
+				is_holiday)
+	FROM '/data/london_calendar.csv'
+	WITH (FORMAT CSV, HEADER);
+
+
+ 			-- **** Creating table of averaged kWh per day energy consumption: *****--
+CREATE TABLE avg_consumption (
+	date			DATE PRIMARY KEY,
+	consumption 	NUMERIC(7,4)
+);
+
+INSERT INTO avg_consumption
+	SELECT date, AVG(kwhr)::NUMERIC(7, 4) AS consumption
+			FROM energy
+			GROUP BY date;
+
+
+ 			-- **** Creating view for DuckDB Feature creation: *****--
+CREATE VIEW v_daily_model_input AS
+	SELECT
+		a.date,
+		a.consumption,
+		w.cloud_cover, w.sunshine, w.global_radiation,
+		w.max_temp, w.mean_temp, w.min_temp,
+		w.precipitation, w.pressure, w.snow_depth,
+		c.dow, c.day, c.month, c.year, c.doy, c.is_weekend, c.is_holiday
+	FROM avg_consumption a
+		JOIN weather w USING (date)
+		JOIN calendar c USING (date);
+
+
+ 			-- **** REQUIRES FEATURES TABLE CREATED WITH DUCKDB: *****--
+
+-- ALTER TABLE features_daily ADD PRIMARY KEY (date);
+-- CREATE INDEX IF NOT EXISTS idx_features_daily_date ON features_daily(date);
+
+-- select * from features_daily ORDER BY date;
